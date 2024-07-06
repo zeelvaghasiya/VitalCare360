ca3245bee2e12e3533d2011db791fc0c34fa684b
zeel vaghasiya <zeel90dv@gmail.com>
2024-07-07 00:14:14 +0530

    set proxy 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://vitalcare360.onrender.com/' // http://localhost:8000
     }
   },
   plugins: [react()],
